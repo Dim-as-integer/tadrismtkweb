a47776bc0bf6adceeae760c0ab0ad562b8c67aa6
Dim-as-integer <Dim.as.long.integer@gmail.com>
2018-08-14 14:11:09 +0700

    Update utama.js

--- utama.js
+++ utama.js
@@ -1,4 +1,5 @@
+window.onload = function () { 
 $('body').css("background-image", "url('https://i.pinimg.com/originals/81/6d/fd/816dfd710806e20aa40a7cd8c76d2e2d.jpg')");
 $("#lowongan").hide();
 $("#a_middle").hide();
-
+}
